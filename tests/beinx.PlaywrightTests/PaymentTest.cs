818a99e7d60b6474c07a58af4d051473fa64a6ed
ipax77 <ph.ilipp@web.de>
2025-11-09 08:58:20 +0100

    add playwright tests

---
+++ tests/beinx.PlaywrightTests/PaymentTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright.MSTest;
+
+namespace beinx.PlaywrightTests;
+
+[TestClass]
+public sealed class PaymentTest : PageTest
+{
+    [TestMethod]
+    public async Task HasTitle()
+    {
+        await Page.GotoAsync("http://localhost:6066/payments");
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync(new Regex("Payments"));
+    }
+}
